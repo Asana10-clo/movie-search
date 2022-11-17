2f5f85309dec1a398290255d3d92e9f023cbb82e
tedashikode <theodoremawuli77@gmail.com>
2022-11-17 18:03:28 +0000

    implemented the search functionality

--- src/App.js
+++ src/App.js
@@ -1,18 +1,61 @@
-
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [search, setSearch] = useState('');
+  const [data, setData] = useState([]);
+
+  
+  //search the api 
+  
+  const fetchData = (search) => { fetch(`http://www.omdbapi.com/?apikey=f8263a62&s=${search}`)
+  .then(res => res.json())
+  .then(data => {
+    setData(data.Search)
+    console.log(data.Search)
+  })
+  }
+  
+  const searchFunction = (e) => { 
+    e.preventDefault();
+    console.log(search);
+    fetchData(search);
+    return data;
+  }
+    // setData(Data) = new Map()
+  //setData(data);
+  // console.log(data.map())
+
   return (
     <div className="App">
       <div className="search-container">
         <h1>Search for your movies here</h1>
           <form>
             <label>Search here...</label>
-            <input className="search-input"></input>
+            <input 
+              value={search} 
+              onChange={(e) => setSearch(e.target.value)}
+              className="search-input"
+              placeholder="Search here..."
+            />
+              
+            <button onClick={searchFunction} type="submit">GO</button>
           </form>
+      </div>
+
+      <div>
+        {data.map((movie , index)=>{
+          return(
+          <div key={index}>
+            <p> {movie.Title} </p>
+          </div>
+        )
+        })}
       </div>
     </div>
   );
 }
+
 
 export default App;
---
+++ src/MovieSearch.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const MovieSearch = () => {
+   
+
+  //get some data 
+  //populate card UI with data gotten
+
+
+  return (
+    <div>
+        MovieSearch
+    </div>
+  )
+}
+
+export default MovieSearch;
