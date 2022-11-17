34988135aa0bdddb08a3737d010b5be471fe2089
tedashikode <theodoremawuli77@gmail.com>
2022-11-17 14:49:47 +0000

    centered stuff on the page

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Search for your movies here</h1>
       <div className="search-container">
-        <form>
-          <label>Search here...</label>
-          <input className="search-input"></input>
-        </form>
+        <h1>Search for your movies here</h1>
+          <form>
+            <label>Search here...</label>
+            <input className="search-input"></input>
+          </form>
       </div>
     </div>
   );
