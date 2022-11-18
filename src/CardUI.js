4c3c3fe48708cfe8148f4d3299658c7ef37f9cfc
Asana10-clo <asanaalhassanat98@gmail.com>
2022-11-18 07:29:10 -0800

    added styles

--- src/CardUI.js
+++ src/CardUI.js
@@ -1,11 +1,11 @@
 function CardUI(props){
     return(
         <div className="bg-gray-700 rounded border-b">
-            <div className="w-16 h-40">
-                <img src={props.img}  alt="movie-poster" />
+            <div className="w-40 h-30 ">
+                <img className="object-cover h-50 w-50 rounded-lg align-middle" src={props.img}  alt="movie-poster" />
             </div>
-            <div>
-                <p>{props.title}</p>
+            <div className='text-red-300'>
+                <p >{props.title}</p>
                 <p>{props.year}</p>
                 <p>{props.id}</p>
                 <p>{props.type}</p>
